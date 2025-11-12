9f43ba4d839e102978d57681a2fdc83d453dd0eb
Berk Yurttaş <yurttasberk@gmail.com>
2025-11-12 11:21:18 +0300

    migration

--- dtbackend/alembic/versions/596a42d100a9_announcement.py
+++ dtbackend/alembic/versions/596a42d100a9_announcement.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '596a42d100a9'
-down_revision = '97ab71a344c8'
+down_revision = '970b2b76e8dd'
 branch_labels = None
 depends_on = None
 
